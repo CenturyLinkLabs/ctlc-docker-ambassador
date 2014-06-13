698957be4fb2107612076c3213c5ca16846c1007
Swen Thümmler <swen.thuemmler@vcsys.de>
2014-06-13 15:50:46 +0200

    be supervisord-friendly by not putting socat into the background

--- start-socat.sh
+++ start-socat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat -ls TCP4-LISTEN:\1,fork,reuseaddr TCP4:\2:\3 \&/'  | sh
+env | grep _TCP= | sed 's/.*_PORT_\([0-9]*\)_TCP=tcp:\/\/\(.*\):\(.*\)/socat -ls TCP4-LISTEN:\1,fork,reuseaddr TCP4:\2:\3/'  | sh
